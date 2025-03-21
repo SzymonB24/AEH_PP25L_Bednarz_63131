92333b5be8ff6b3bcd337d18e1f107133e6a818c
SzymonB24 <sz.bednarz.2004@gmail.com>
2025-03-21 11:31:18 +0100

    Zapis do repozytorium Lab3 poprawione

--- src/pl/pp/mojaTrzeciaAplikacja.java
+++ src/pl/pp/mojaTrzeciaAplikacja.java
@@ -6,18 +6,16 @@ class mojaTrzeciaAplikacja {
 
         Scanner scanner = new Scanner(System.in);
 
+        System.out.println("Podaj proszę liczbę dni, którą chcesz przeliczyć: ");
+
         while (true) {
 
-
             //zamiana dni na tygodnie
-            System.out.println("Podaj proszę liczbę dni, którą chcesz przeliczyć na tygodnie: ");
-
-
             int x = scanner.nextInt();
             int y, z;
 
             if (x <= 0) {
-                System.out.println("podałeś błędną wartość. Zamykanie programu...");
+                System.out.println("Zamykanie programu...");
                 break;
             }
 
@@ -26,10 +24,30 @@ class mojaTrzeciaAplikacja {
                 y = x / 7;    //oblicznie tygodni
                 z = x % 7;    // reszta dni
                 System.out.println(x + " dni to " + y + " tygodnie i " + z + " dni");
+                System.out.println("Spróbuj ponownie lub podaj wartość ujemną aby zakończyć: ");
             }
 
         }
-    scanner.close();
+        while (true) {
+            System.out.println("Podaj temperaturę w stopniach Fahrenheita : ");
+
+            double F = scanner.nextDouble();  // Wczytanie stopni Fahrenheita
+
+            if (F == -1) {  // zakończenie programu
+                System.out.println("Zamykanie...");
+                break;
+            }
+
+            // Obliczenie temperatur
+            double C = (F - 32) / 1.8;        //stopnie Celcjusza
+            double K = C + 273.15;            // stopnie Kelwina
+
+            // Wyświetlenie wyników
+            System.out.println("Twoja temperatura " + F + " stopni Fahrenheita to:");
+            System.out.printf("%.2f stopni Celsjusza%n", C);
+            System.out.printf("%.2f stopni Kelwina%n", K);
+            System.out.println("jeśli chcesz zakończyć wpisz -1");
+        }
     }
 }
 
