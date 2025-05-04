0f3dafa3b3553b98e071031a453ebed4396d15c1
SzymonB24 <sz.bednarz.2004@gmail.com>
2025-05-04 12:11:52 +0200

    Lab8 zadanie.

---
+++ src/mojaDziewiataAplikacja.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class mojaDziewiataAplikacja {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Magazyn magazyn = new Magazyn("Tomek","tomek1998@gmail.com","792877553","236",5000 );
+        System.out.println("Witaj w programie do zarządzania Twoim magazynem. Magazyn ma pojemność 10000 jednostek." +
+                " Obecnie wolne jest " + magazyn.getSpace() + " jednostek.");
+        magazyn.dodajTowar(2000);
+        magazyn.dodajTowar(5000);
+        System.out.println(magazyn.getSpace());
+        magazyn.usunTowar(6000);
+        magazyn.sprawdzZajetosc();
+        magazyn.aktualizujKontakt();
+        magazyn.sprawdzZajetosc();
+
+    }
+}
